a9dbd1a2e8dc9598e46c9d389ef3a4f5ad59ac19
Joost Rijneveld <joostrijneveld@gmail.com>
2012-02-20 17:43:20 +0100

    Implementatie van k-NN en bijhouden van aantal correcte beweringen

--- src/Sample.java
+++ src/Sample.java
@@ -4,23 +4,58 @@
  */
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
  * @author Joost
  */
-public class Sample {
+public class Sample implements Comparable<Sample> {
     
-    public Sample() {
-        
-    }
-
+	private ArrayList<Double> attributes;
+	private int classification;
+	private double tmpdistance = 0;
+	
     public Sample(ArrayList<Double> attributes, int classification) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        this.attributes = attributes;
+        this.classification = classification;
+    }
+    
+    /**
+     * Berekent simpelweg de Eucledian distance tussen twee lijsten met attributes
+     * @param secondatts Een lijst met attributes evenlang als die van Sample
+     * @return de distance als double
+     * @throws Exception wordt gegooid als de lijsten geen overeenkomstige lengte hebben
+     */
+    public double distance(Sample s) throws Exception {
+    	if (s.attributes.size() != attributes.size()) {
+    		throw new Exception("Attribute counts do not match");
+    	}
+    	double dist = 0;
+    	Iterator<Double> it1 = s.attributes.iterator();
+    	Iterator<Double> it2 = attributes.iterator();
+    	while (it1.hasNext()) {
+    		dist += Math.pow(Math.abs(it1.next() - it2.next()),2);
+    	}
+    	return Math.sqrt(dist);
     }
 
-    boolean isCorrect() {
-        throw new UnsupportedOperationException("Not yet implemented");
+	public int getClassification() {
+    	return classification;
     }
+    
+    public double getTmpDistance() {
+    	return tmpdistance;
+    }
+    
+    public void setTmpDistance(double d) {
+    	tmpdistance = d;
+    }
+
+	public int compareTo(Sample arg0) {
+		if (tmpdistance < arg0.getTmpDistance())
+			return -1;
+		return (tmpdistance == arg0.getTmpDistance() ? 0 : -1);
+	}
     
 }
--- src/Specimen.java
+++ src/Specimen.java
@@ -1,9 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
@@ -11,14 +7,55 @@ import java.util.ArrayList;
  */
 public class Specimen {
     
-    private ArrayList<Integer> attributes;
-    private ArrayList<Integer> examples;
-    private ArrayList<Sample> samples;
-    
-    public Specimen (ArrayList<Integer> a, ArrayList<Integer> e, ArrayList<Sample> s) {
-        attributes = a;
-        examples = e;
-        samples = s;
+	public static final int K = 3;
+	
+    private final ArrayList<Integer> attributes;
+    private final ArrayList<Integer> examples;
+    private final ArrayList<Sample> samples;
+    private int correct;
+
+    public Specimen (ArrayList<Integer> attributes, ArrayList<Integer> examples, ArrayList<Sample> samples) {
+        this.attributes = attributes;
+        this.examples = examples;
+        this.samples = samples;
+        correct = 0;
+        try {
+	        for (Sample sample:this.samples) {
+	        	ArrayList<Sample> referenceSet = new ArrayList<Sample>();
+	        	/* TODO dit moet efficienter kunnen, want examples en samples zijn allebei gesorteerd,
+	        	dus dat algoritme van bij Complexiteit met O(k * n) ipv O(n^2) werkt hier.
+	        	Maar nu even geen zin in.*/
+	        	for (Integer example:examples) {
+	        		Sample t = samples.get(example);
+	        		t.setTmpDistance(sample.distance(t));
+	        		referenceSet.add(t);
+	        	}
+	        	//worden dus gesorteerd op distance, want Sample implementeert het Comparable-interface.
+	        	Collections.sort(referenceSet);
+	        	//alleen de eerste K elementen overhouden (door sublist te maken en die te clearen)
+	        	referenceSet.subList(K, referenceSet.size()).clear();
+	        	int maxfreq = 0;
+	        	int maxclass = -1;
+	        	//om alle classes af te lopen
+	        	for (Sample s:referenceSet) {
+	        		int freq = 0;
+	        		for (Sample s2:referenceSet) {
+	        			if (s.getClassification() == s2.getClassification()) {
+	        				freq++;
+	        			}
+	        		}
+	        		if (freq > maxfreq) {
+	        			maxclass = s.getClassification();
+	        		}
+	        	}
+	            if (sample.getClassification() == maxclass) {
+	                correct++;
+	            }
+	        }
+	    }
+        catch (Exception e1) {
+			e1.printStackTrace();
+		}
     }
     
     public ArrayList<Integer> getExamples() {
@@ -29,14 +66,16 @@ public class Specimen {
         return attributes;
     }
     
+    public int getSampleCount() {
+    	return samples.size();
+    }
+    
+    public int getAttributeCount() {
+    	return attributes.size();
+    }
+    
     public int getCorrectSamples() {
-        int total = 0;
-        for (Sample sample:samples) {
-            if (sample.isCorrect()) {
-                total++;
-            }
-        }
-        return total;
+        return correct;
     }
     
 
