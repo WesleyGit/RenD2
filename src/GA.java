27434ed105e382d9e3f7879620e2821ac022a834
Joost Rijneveld <joostrijneveld@gmail.com>
2012-02-29 20:28:40 +0100

    Even de tabs gefixt

--- src/GA.java
+++ src/GA.java
@@ -23,8 +23,8 @@ public class GA {
 		//Proberen het bestand in te lezen..
         try
         {
-            //FileReader file = new FileReader("trainmnist.txt");
-        	FileReader file = new FileReader("train.txt");
+            FileReader file = new FileReader("mnisttrain.txt");
+        	//FileReader file = new FileReader("train_set_001_n100_err0.txt");
             Scanner scan = new Scanner( file );
             String line;
             int no = 0;
@@ -51,7 +51,7 @@ public class GA {
 
 	private ArrayList<Specimen> population;
 	private GeneticStrategy strategy;
-	private static final int ITERATIONS = 10;
+	private static final int ITERATIONS = 500;
 	private static final int POPSIZE = 50;
 	
 	public GA(ArrayList<Sample> trainset, GeneticStrategy strategy) {
@@ -85,48 +85,48 @@ public class GA {
     
 	private static void createResults(Specimen classifier, ArrayList<Sample> trainset) {
 		try {
-			 	// Maken van prototype-file
-				  FileWriter ofstream = new FileWriter("prototypes.txt");
-				  BufferedWriter out = new BufferedWriter(ofstream);
-				  for (int e : classifier.getExamples()) {
-					  Sample s = trainset.get(e);
-					  Iterator<Integer> iteratorA = classifier.getAttributes().iterator();
-					  int a = iteratorA.hasNext() ? iteratorA.next() : -1;
-					  for (int i = 0; i < s.getAttributeCount(); i++) {
-						  if (a > i) {
-							  continue;
-						  }
-						  else if (a == i) {
-							  out.write(s.getAttributeValue(i)+" ");
-							  a = iteratorA.hasNext() ? iteratorA.next() : -1;
-						  }
-					  }
-					  out.write(s.getClassification()+"\n");
-				  }
-				  //Sluiten van schrijven naar prototype-file
-				  out.close();
-		 }
-		 catch (Exception e) {//Catch exception if any
-			  System.err.println("Error: " + e.getMessage());
-		 }
+			// Maken van prototype-file
+			FileWriter ofstream = new FileWriter("prototypes.txt");
+			BufferedWriter out = new BufferedWriter(ofstream);
+			for (int e : classifier.getExamples()) {
+				Sample s = trainset.get(e);
+				Iterator<Integer> iteratorA = classifier.getAttributes().iterator();
+				int a = iteratorA.hasNext() ? iteratorA.next() : -1;
+				for (int i = 0; i < s.getAttributeCount(); i++) {
+					if (a > i) {
+						continue;
+					}
+					else if (a == i) {
+						out.write(s.getAttributeValue(i)+" ");
+						a = iteratorA.hasNext() ? iteratorA.next() : -1;
+					}
+				}
+				out.write(s.getClassification()+"\n");
+			}
+			//Sluiten van schrijven naar prototype-file
+			out.close();
+		}
+		catch (Exception e) {//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
 		 
 		ArrayList<Sample> testset = new ArrayList<Sample>();
 		try {
-			  FileReader ifstream = new FileReader("test.txt");
-			  Scanner scan = new Scanner( ifstream );
-			  String line;
-				int no = 0;
-				System.out.println("Start reading testset");
-				while (scan.hasNextLine()) {
-					line = scan.nextLine();
-					String[] input = line.split(" ");
-					ArrayList<Double> att = new ArrayList<Double>();
-					for (int i = 0; i < input.length; i++) {
-						att.add(Double.valueOf(input[i]));
-					}
-				    testset.add(new Sample(att, classifier.computeClassification(att), no++));
+			FileReader ifstream = new FileReader("test_data_001_n100.txt");
+			Scanner scan = new Scanner( ifstream );
+			String line;
+			int no = 0;
+			System.out.println("Start reading testset");
+			while (scan.hasNextLine()) {
+				line = scan.nextLine();
+				String[] input = line.split(" ");
+				ArrayList<Double> att = new ArrayList<Double>();
+				for (int i = 0; i < input.length; i++) {
+					att.add(Double.valueOf(input[i]));
 				}
-				ifstream.close();
+				testset.add(new Sample(att, classifier.computeClassification(att), no++));
+			}
+			ifstream.close();
 		}
 		catch (Exception e) {//Catch exception if any
 			System.err.println("Error: " + e.getMessage());
@@ -149,5 +149,4 @@ public class GA {
 		
 		System.out.println("Done!");
 	}
-    
 }
